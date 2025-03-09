33ea545285c7b2d12f82a880af11575a75d19384
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 17:09:53 +0900

    file choose

--- parser/src/main.ts
+++ parser/src/main.ts
@@ -1,6 +1,11 @@
 import { AtomParser } from './core/parser';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const main = () => {
+  const SOURCE_DIR = './source';
+  const atomFileNames = fs.readdirSync(path.join('.', SOURCE_DIR)).filter((file) => file.endsWith('.atom'));
+  const atomFiles = atomFileNames.map((fileName) => fs.readFileSync(path.join('.', SOURCE_DIR, fileName), 'utf-8'));
   const atomParser = new AtomParser('');
 };
 
