94c28a6ed55e86c5955eef5b4535c49a3456992b
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 23:52:14 +0900

    schema追加

--- add-index/src/model/document.ts
+++ add-index/src/model/document.ts
@@ -1,3 +1,10 @@
 import { z } from 'zod';
 
-const schema = z.object({});
+const schema = z.object({
+  id: z.string(),
+  title: z.string(),
+  summary: z.string(),
+  link: z.string(),
+  author: z.string(),
+  issued: z.string(),
+});
