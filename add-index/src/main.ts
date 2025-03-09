2a3fda0207481a936ed2e5ee7c5e7ad9cb804fb4
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 18:05:10 +0900

    healthcheck

--- add-index/src/main.ts
+++ add-index/src/main.ts
@@ -1,1 +1,12 @@
-console.log(1);
+import { Client } from '@opensearch-project/opensearch';
+
+const main = async () => {
+  const client = new Client({
+    node: `https://admin:F50UjL0vRYRR8o&*@localhost:9200`,
+    ssl: { rejectUnauthorized: false },
+  });
+  const health = await client.cluster.health();
+  console.log(health);
+};
+
+void main();
