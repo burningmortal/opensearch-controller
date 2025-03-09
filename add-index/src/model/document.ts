c2e206963c5880c2e7f443b2daab6c56cc7f6c6b
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-10 00:09:07 +0900

    バリデーター追加

--- add-index/src/model/document.ts
+++ add-index/src/model/document.ts
@@ -4,7 +4,25 @@ const schema = z.object({
   id: z.string(),
   title: z.string(),
   summary: z.string(),
-  link: z.string(),
+  link: z.string().url(),
   author: z.string(),
-  issued: z.string(),
 });
+
+export type SearchDocument = z.infer<typeof schema>;
+
+export type DocumentValidateResult = { isOk: true; value: SearchDocument } | { isOk: false };
+
+export const validateDocument = (value: any): DocumentValidateResult => {
+  const parsed = schema.safeParse({
+    id: value?.id,
+    title: value?.title,
+    summary: value?.summary,
+    link: value?.link,
+    author: value?.author,
+  });
+  if (!parsed.success) {
+    return { isOk: false };
+  }
+  const document = parsed.data;
+  return { isOk: true, value: document };
+};
