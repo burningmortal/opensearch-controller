10fd5b3485f56ed9e4822d05bb0a394658c9192b
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 17:56:28 +0900

    解析結果を出力する処理追加

--- parser/src/main.ts
+++ parser/src/main.ts
@@ -1,9 +1,11 @@
+import { randomUUID } from 'crypto';
 import { AtomParser } from './core/parser';
 import * as fs from 'fs';
 import * as path from 'path';
 
 const main = async () => {
   const SOURCE_DIR = './source';
+  const DEST_DIR = './dest';
   const atomFileNames = fs.readdirSync(path.join('.', SOURCE_DIR)).filter((file) => file.endsWith('.atom'));
   const atomFiles = atomFileNames.map((fileName) => fs.readFileSync(path.join('.', SOURCE_DIR, fileName), 'utf-8'));
   await Promise.all(
@@ -13,6 +15,19 @@ const main = async () => {
       if (!result) {
         return;
       }
+      const rows = result.map((res) =>
+        [
+          `"${res.id.replace(/"/, '\\"').replace(/\n/g, ' ')}"`,
+          `"${res.title.replace(/"/, '\\"').replace(/\n/g, ' ')}"`,
+          `"${res.summary.replace(/"/, '\\"').replace(/\n/g, ' ')}"`,
+          `"${res.link.replace(/"/, '\\"').replace(/\n/g, ' ')}"`,
+          `"${res.author.replace(/"/, '\\"').replace(/\n/g, ' ')}"`,
+        ].join(','),
+      );
+      const csv = rows.join('\n');
+
+      fs.existsSync(path.join('.', DEST_DIR)) || fs.mkdirSync(path.join('.', DEST_DIR));
+      fs.writeFileSync(path.join('.', DEST_DIR, `result_${randomUUID()}.csv`), csv);
     }),
   );
 };
