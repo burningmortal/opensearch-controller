c90c1dcc3c203f1d4ee2d6794ffa13531b35499c
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 21:36:24 +0900

    キャスト追加

--- add-index/src/core/config/schema.ts
+++ add-index/src/core/config/schema.ts
@@ -2,10 +2,20 @@ import { z } from 'zod';
 
 const schema = z.object({
   opensearchProtocol: z.string(),
+  opensearchUsername: z.string(),
+  opensearchPassword: z.string(),
+  opensearchHostname: z.string(),
+  opensearchRejectAuthorized: z.enum(['true', 'false']).transform((val) => val === 'true'),
+  opensearchPort: z.number(),
 });
 
 const parsedEnv = schema.safeParse({
   opensearchProtocol: process.env.OPENSEARCH_PROTOCOL,
+  opensearchUsername: process.env.OPENSEARCH_USERNAME,
+  opensearchPassword: process.env.OPENSEARCH_PASSWORD,
+  opensearchHostname: process.env.OPENSEARCH_HOSTNAME,
+  opensearchRejectAuthorized: process.env.OPENSEARCH_REJECT_AUTHORIZED,
+  opensearchPort: z.coerce.number().parse(process.env.OPENSEARCH_PORT),
 });
 
 if (!parsedEnv.success) {
--- add-index/src/main.ts
+++ add-index/src/main.ts
@@ -3,8 +3,8 @@ import { env } from './core/config/schema';
 
 const main = async () => {
   const client = new Client({
-    node: `${env.opensearchProtocol}://${process.env['OPENSEARCH_USERNAME']}:${process.env['OPENSEARCH_PASSWORD']}@${process.env['OPENSEARCH_HOSTNAME']}:${process.env['OPENSEARCH_PORT']}`,
-    ssl: { rejectUnauthorized: process.env['OPENSEARCH_REJECT_AUTHORIZED'] === 'true' },
+    node: `${env.opensearchProtocol}://${env.opensearchUsername}:${env.opensearchPassword}@${env.opensearchHostname}:${env.opensearchPort}`,
+    ssl: { rejectUnauthorized: env.opensearchRejectAuthorized },
   });
   const health = await client.cluster.health();
   console.log(health);
