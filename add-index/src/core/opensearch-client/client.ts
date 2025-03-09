755fa526a0862d1d1d3e38cd7af243b098dab83a
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-09 22:08:00 +0900

    ヘルスチェック実装

--- add-index/src/core/opensearch-client/client.ts
+++ add-index/src/core/opensearch-client/client.ts
@@ -1,6 +1,14 @@
 import { Client, errors } from '@opensearch-project/opensearch';
+import { Cluster_Health_Response } from '@opensearch-project/opensearch/api';
 
 export type Options = { ssl: { rejectUnauthorized: boolean } };
+
+export type RequestResult<T> =
+  | { isOk: true; value: T }
+  | {
+      isOk: false;
+      error: { type: 'ConnectionError'; name: string; message: string } | { type: 'Unknown' };
+    };
 
 export class OpenSearchClient {
   private client: Client;
@@ -9,13 +17,15 @@ export class OpenSearchClient {
     this.client = new Client({ node, ...options });
   }
 
-  health() {
+  async health(): Promise<RequestResult<Cluster_Health_Response>> {
     try {
-      return this.client.cluster.health();
+      const res = await this.client.cluster.health();
+      return { isOk: true, value: res };
     } catch (e) {
       if (e instanceof errors.ConnectionError) {
-        return false;
+        return { isOk: false, error: { type: 'ConnectionError', name: e.name, message: e.message } };
       }
+      throw { isOk: false, error: { type: 'Unknown' } };
     }
   }
 }
--- add-index/src/main.ts
+++ add-index/src/main.ts
@@ -2,13 +2,9 @@ import { env } from './core/config/schema';
 import { OpenSearchClient } from './core/opensearch-client/client';
 
 const main = async () => {
-  const client = new OpenSearchClient(
-    `${env.opensearchProtocol}://${env.opensearchUsername}:${env.opensearchPassword}@${env.opensearchHostname}:${env.opensearchPort}`,
-    { ssl: { rejectUnauthorized: env.opensearchRejectAuthorized } },
-  );
-
+  const node = `${env.opensearchProtocol}://${env.opensearchUsername}:${env.opensearchPassword}@${env.opensearchHostname}:${env.opensearchPort}`;
+  const client = new OpenSearchClient(node, { ssl: { rejectUnauthorized: env.opensearchRejectAuthorized } });
   const health = await client.health();
-  console.log(health);
 };
 
 void main();
