6b1b000e337b2b93b90ad12181c48a503b58cb2b
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-10 23:44:15 +0900

    add pydantic

--- sentence-transformers-api/main.py
+++ sentence-transformers-api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 @app.get("/")
 def health():
