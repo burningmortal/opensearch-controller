5368166ae0ee0a32da579ae13c9cb7324dd106c0
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-10 23:47:22 +0900

    add encode

--- sentence-transformers-api/main.py
+++ sentence-transformers-api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+class TextInput(BaseModel):
+  text: str
+
 @app.get("/")
 def health():
   return {"health": "ok"}
+
+@app.post("/encode")
+async def encode_text(input: TextInput):
+  embedding = model.encode(input.text)
+  return {"embedding": embedding.tolist()}
