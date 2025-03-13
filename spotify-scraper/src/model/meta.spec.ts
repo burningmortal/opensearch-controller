31b7b161a37052f416e57be2a20d8d8e48a7f983
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-13 22:52:02 +0900

    ケース追加

--- spotify-scraper/src/model/meta.spec.ts
+++ spotify-scraper/src/model/meta.spec.ts
@@ -15,3 +15,33 @@ test('test', () => {
     owner: '',
   });
 });
+
+const ParseBoolResult = {
+  成功: true,
+  失敗: false,
+} as const;
+
+const searchKeyByValue = <T, U extends object>(value: T, def: U) => {
+  const target = Object.entries(def).find(([k, v]) => v === value);
+  if (!target) {
+    return undefined;
+  }
+  return target[0];
+};
+
+describe('parseMeta', () => {
+  it.todo('createdAtに`yyyy-MM-ddTHH:mm:ss.SSSZ`の書式を満たす文字列を与えるとバリデーションに成功すること');
+  it.each`
+    key            | valid                                                      | result
+    ${'createdAt'} | ${searchKeyByValue(ParseBoolResult.成功, ParseBoolResult)} | ${searchKeyByValue(ParseBoolResult.成功, ParseBoolResult)}
+    ${'createdAt'} | ${searchKeyByValue(ParseBoolResult.失敗, ParseBoolResult)} | ${searchKeyByValue(ParseBoolResult.失敗, ParseBoolResult)}
+    ${'updatedAt'} | ${searchKeyByValue(ParseBoolResult.成功, ParseBoolResult)} | ${searchKeyByValue(ParseBoolResult.成功, ParseBoolResult)}
+    ${'updatedAt'} | ${searchKeyByValue(ParseBoolResult.失敗, ParseBoolResult)} | ${searchKeyByValue(ParseBoolResult.失敗, ParseBoolResult)}
+  `('{$key}に`yyyy-MM-ddTHH:mm:ss.SSSZ`の書式を{$valid}文字列を与えるとバリデーションに{$result}すること', () => {
+    return;
+  });
+
+  it.todo('ownerに1000文字以内の文字列が入るとバリデーションに成功すること');
+  it.todo('ownerに1001文字の文字列が入るとバリデーションに失敗すること');
+  it.todo('ownerに空文字が入るとバリデーションに失敗すること');
+});
