dc4c6d456cd73d68e1f82ebfed675cbc946c38c5
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-13 21:33:43 +0900

    get albums

--- spotify-scraper/src/main.ts
+++ spotify-scraper/src/main.ts
@@ -1,12 +1,16 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 
 const main = async () => {
-  console.log('look at me');
-  new SpotifyWebApi({
-    clientId: '',
-    clientSecret: '',
-    redirectUri: '',
+  const spotify = new SpotifyWebApi({
+    clientId: process.env['SPOTIFY_CLIENT_ID'],
+    clientSecret: process.env['SPOTIFY_CLIENT_SECRET'],
+    redirectUri: process.env['SPOTIFY_REDIRECT_URI'],
   });
+
+  const data = await spotify.clientCredentialsGrant();
+  spotify.setAccessToken(data.body['access_token']);
+  const artists = await spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE');
+  console.log(artists.body);
 };
 
 main();
