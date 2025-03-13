afec2bb72e964c21d8b7c01a4cc5d98040debc86
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-13 22:30:18 +0900

    fix config

--- spotify-scraper/jest.config.js
+++ spotify-scraper/jest.config.js
@@ -125,9 +125,7 @@ const config = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ['<rootDir>/src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -174,7 +172,9 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
