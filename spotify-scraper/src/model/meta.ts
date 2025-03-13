814e376995a007320da8711a6392e6cf4024f870
もえるいのち <103842124+burningmortal@users.noreply.github.com>
2025-03-14 00:31:16 +0900

    ISO8601正規表現追加

--- spotify-scraper/src/model/meta.ts
+++ spotify-scraper/src/model/meta.ts
@@ -2,7 +2,12 @@ import { z } from 'zod';
 
 export const metaSchema = z
   .object({
-    createdAt: z.string(),
+    createdAt: z
+      .string()
+      .regex(
+        /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])\.[0-9]{3}Z$/,
+        'ISO8601形式のUTCで入力してください (ex. `yyyy-MM-ddTHH:MM:ss.SSSZ`)',
+      ),
     updatedAt: z.string(),
     owner: z.string(),
   })
